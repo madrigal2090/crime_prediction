5483e78b9f50e9d4724d8127b62e83a226247e61
Francisco J Madrigal <41655265+madrigal2090@users.noreply.github.com>
2022-01-09 21:20:45 -0600

    commit_app

--- app.py
+++ app.py
@@ -1,142 +1,61 @@
 
 import os
 import pandas as pd
+import numpy as np
 import geopandas as gpd
 import plotly.graph_objects as go
 import dash
 #from dash import html, dcc
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
+
+## Read Crimes Probabilities DataFrame and transform
+results_brf = pd.read_csv(r"modelo\df_results\reduced_results_brf.csv")
+colonias = pd.read_csv(r"modelo\df_results\reduced_colonias.csv")
+
+results_brf = results_brf.merge(colonias[['id_colonia', 'colonia']],
+                                how='left',
+                                on='id_colonia')
 
 
-"""
-## Define your path
-PATH = r"D:\francisco_madrigal\Desktop\Tesis\modelo"
+colonias['geometry'] = gpd.GeoSeries.from_wkt(colonias['geometry'])
 
-def create_path(file_path, path=PATH):
-    
-    return os.path.join(path, file_path)
+colonias = gpd.GeoDataFrame(colonias, geometry='geometry')
 
+colonias = colonias.set_crs(epsg=4326, inplace=True)
 
-## Read neighborhoods DataFrame for later use
-colonias = gpd.read_file(create_path(r"colonias_fixed\colonias_fixed.shp"))
+colonias = colonias[['id_colonia', 'geometry']].__geo_interface__
 
-## Declare the used coordinate system
-colonias.crs = "epsg:4326"
+results_brf = results_brf.sort_values(by='day_period')
 
-## Read Crimes Probabilities DataFrame adn transform
-results_brf = pd.read_csv(create_path(r"df_results\results_brf.csv"), low_memory=False,
-                          parse_dates =['Hora'])
-
-results_brf['geometry'] = gpd.GeoSeries.from_wkt(results_brf['geometry'])
-
-results_brf = gpd.GeoDataFrame(results_brf, geometry='geometry')
-
-
-mean_dayp = results_brf.groupby(['id_colonia', 'day_period'])[['proba_crimen']]\
-    .mean().reset_index()
-
-mean_dayp['day_period'].replace({0:'early_morning',
-                                 1:'morning',
-                                 2:'afternoon',
-                                 3:'evening'},
+results_brf['day_period'].replace({0:'Early morning [0 to 6 hours)',
+                                  1:'Morning [6 to 12 hours)',
+                                  2:'Afternoon [12 to 18 hours)',
+                                  3:'Evening [18 to 24 hours)'},
                                inplace=True)
 
-mean_dayp = mean_dayp.pivot_table(index=['id_colonia'], columns='day_period',
-                                  values='proba_crimen', aggfunc='first').reset_index()
+day_period = results_brf['day_period'].unique()
 
-mean_dayp = mean_dayp.merge(colonias[['id_colonia', 'colonia', 'alcaldi', 'geometry']],
-                            how='left',
-                            on='id_colonia')
+day_period = np.insert(day_period, 0, 'Aggregate')
 
-"""
-mean_dayp = pd.read_csv(r"/home/fjmadrigal/mysite/mean_predict_graph_prueba.csv")
+results_brf = results_brf.sort_values(by='dia_semana')
 
-mean_dayp['geometry'] = gpd.GeoSeries.from_wkt(mean_dayp['geometry'])
+results_brf['dia_semana'].replace({0:'Sunday',
+                                  1:'Monday',
+                                  2:'Tuesday',
+                                  3:'Wednesday',
+                                  4:'Thursday',
+                                  5:'Friday',
+                                  6:'Saturday'},
+                               inplace=True)
 
-mean_dayp = gpd.GeoDataFrame(mean_dayp, geometry='geometry')
+dia_semana = results_brf['dia_semana'].unique()
 
-
-mean_dayp = mean_dayp.set_crs(epsg=4326, inplace=True)
-
-mean_daypjson = mean_dayp[['id_colonia', 'geometry']].__geo_interface__
-
-fig = go.Figure(go.Choroplethmapbox(geojson=mean_daypjson,
-                                    locations=mean_dayp['id_colonia'], 
-                                    z=mean_dayp['morning'],
-                                    customdata=mean_dayp['colonia'],
-                                    colorscale="Viridis",
-                                    marker_opacity=0.7,
-                                    zmax=1, zmin=0,
-                                    name='',
-                                    featureidkey="properties.id_colonia",
-                                    hovertemplate= '<b>%{customdata} </b><br><br>Probability=%{z}'
-                   )
-               )
-# fig.update_geos(fitbounds="locations", visible=False)
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-fig.update_layout(mapbox=dict(style='carto-positron',
-                              zoom=9.9, 
-                              center = {"lat": 19.355, "lon": -99.18})) 
-
-fig.update_geos(fitbounds="locations", visible=False)
-
-fig.update_layout(
-    width=1500,
-    height=800,
-    autosize=False,
-    margin=dict(t=100, b=0, l=0, r=0),
-)
+dia_semana = np.insert(dia_semana, 0, 'Aggregate')
 
 
-button1 = dict(method='update',
-              label='Morning [6 to 12 hours)',
-              args=[{'z':[mean_dayp['morning']]},
-                   {'colorscale':'Viridis'}])
-                
-button2 = dict(method='update',
-              label='Afternoon [12 to 18 hours)',
-              args=[{'z':[mean_dayp['afternoon']]},
-                   {'colorscale':'Viridis'}])
-                
-    
-button3 = dict(method='update',
-              label='Evening [18 to 24 hours)',
-              args=[{'z':[mean_dayp['evening']]},
-                   {'colorscale':'Viridis'}])
-
-button4 = dict(method='update',
-              label='Early morning [0 to 6 hours)',
-              args=[{'z':[mean_dayp['early_morning']]},
-                   {'colorscale':'Viridis'}])
-
-fig.update_layout(updatemenus=[dict(active=0,
-                                    buttons=[button1, button2, button3, button4],
-                                    direction="down",
-                                    pad={"r": 10, "t": 10},
-                                    showactive=True,
-                                    x=0.01,
-                                    xanchor="left",
-                                    y=1.1,
-                                    yanchor="top")]
-                 )
-
-fig.update_layout(
-    annotations=[
-        dict(text="Time of the day:", showarrow=False,
-        x=0.01, y=1.12, yref="paper", align="left")
-    ]
-)
-
-
-#fig.show()
-#fig.write_html(create_path("map.html"))
-
-# http://127.0.0.1:8050/
-# https://dash.plotly.com/deployment
-# https://plotly.com/python/scattermapbox/
-
+## Create dash plot
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
@@ -152,14 +71,105 @@ app.layout = html.Div([
                  'color': '#444444'
                  }
              ),
+        
+    html.Br(),
     
-    dcc.Graph(figure=fig)
-])
+    html.Div(children=[
+        
+        html.Label(['Select day of the week:'], 
+                   style={'font-weight': 'bold', "text-align": "center"}),
+        
+        dcc.Dropdown(
+            id='dia_semana-column',
+            options=[{'label': i, 'value': i} for i in dia_semana],
+            value='Aggregate',
+            clearable=False, searchable=False
+            ),
+        ], style={'width': '20%'}
+        ),
+
+    html.Div(children=[
+        
+        html.Label(['Select time of the day'], 
+                   style={'font-weight': 'bold', "text-align": "center"}),
+        dcc.Dropdown(
+            id='day_period-column',
+            options=[{'label': i, 'value': i} for i in day_period],
+            value='Aggregate',
+            clearable=False, searchable=False
+        ),
+    ], style={'width': '20%'}
+        
+        ),
+    
+    dcc.Graph(id='map-graph')
+    ])
+    
+
+@app.callback(
+    Output('map-graph', 'figure'),
+    Input('dia_semana-column', 'value'),
+    Input('day_period-column', 'value'))
+def update_graph(dia_semana_val, day_period_val):
+    
+    if dia_semana_val == 'Aggregate' and day_period_val == 'Aggregate':
+        
+        df = results_brf.groupby(['id_colonia', 'colonia'])[['proba_crimen']]\
+        .mean().reset_index()
+        
+    if dia_semana_val != 'Aggregate' and day_period_val == 'Aggregate':
+        
+        df = results_brf.groupby(['id_colonia', 'colonia', 'dia_semana'])[['proba_crimen']]\
+            .mean().reset_index()
+            
+        df = df[df['dia_semana'] == dia_semana_val]
+        
+        
+    if dia_semana_val == 'Aggregate' and day_period_val != 'Aggregate':
+        
+        df = results_brf.groupby(['id_colonia', 'colonia', 'day_period'])[['proba_crimen']]\
+            .mean().reset_index()
+            
+        df = df[df['day_period'] == day_period_val]
+        
+    if dia_semana_val != 'Aggregate' and day_period_val != 'Aggregate':
+        
+        df = results_brf.groupby(['id_colonia', 'colonia', 'day_period', 'dia_semana'])\
+            [['proba_crimen']].mean().reset_index()
+            
+        df = df[(df['day_period'] == day_period_val) & (df['dia_semana'] == dia_semana_val)]
+
+
+    fig = go.Figure(go.Choroplethmapbox(geojson=colonias,
+                                        locations=df['id_colonia'], 
+                                        z=df['proba_crimen'],
+                                        customdata=df['colonia'],
+                                        colorscale="Viridis",
+                                        marker_opacity=0.7,
+                                        zmax=1, zmin=0, zauto=False,
+                                        colorbar_title = "<b>Crime<br>Probability</b>",
+                                        name='',
+                                        featureidkey="properties.id_colonia",
+                                        hovertemplate= '<b>%{customdata} </b><br><br>Probability=%{z}'
+                       )
+                   )
+    # fig.update_geos(fitbounds="locations", visible=False)
+    #fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_layout(margin={'l': 40, 'b': 40, 't': 10, 'r': 0}, hovermode='closest')
+
+    fig.update_layout(mapbox=dict(style='carto-positron',
+                                  zoom=9.9, 
+                                  center = {"lat": 19.355, "lon": -99.18})) 
+
+    fig.update_geos(fitbounds="locations", visible=False)
+
+    fig.update_layout(
+        width=1500,
+        height=800,
+        autosize=False,
+    )
+
+    return fig
 
 #if __name__ == '__main__':
 #app.run_server(debug=True)
-
-
-# https://dash.plotly.com/dash-core-components/datepickerrange
-# https://dash.plotly.com/basic-callbacks
-# https://towardsdatascience.com/plotly-dash-from-development-to-deployment-c9500d16581a
